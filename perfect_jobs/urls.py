7e22e00a78fe3d6d3b36a478dd96ece840a6bcb3
Mac <mac@Macs-MacBook-Pro.local>
2026-02-10 12:39:07 +0200

    path(, home added)

--- perfect_jobs/urls.py
+++ perfect_jobs/urls.py
@@ -16,6 +16,7 @@ schema_view = get_schema_view(
 
 #page for app urls
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
