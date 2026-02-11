5a40c670df835d2eab87c0fcb492788be1bc7330
Sipho Nzilwane <64207098+Msaination@users.noreply.github.com>
2026-02-11 23:07:44 +0200

    Update views.py

--- apps/applications/views.py
+++ apps/applications/views.py
@@ -6,7 +6,7 @@ from .serializers import ApplicationSerializer
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     def get_queryset(self):
         # Users can only see their own applications
         return self.queryset.filter(applicant=self.request.user)
